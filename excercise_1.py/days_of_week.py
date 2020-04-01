29a2d8a422527709cac4eca13269ea24384571f6
Park Jack <parker.jackson.hhs@gmail.com>
2020-04-01 15:13:05 -0400

    days of the week excercise

---
+++ excercise_1.py/days_of_week.py
@@ -0,0 +1,28 @@
+day_of_week = int(input('Day (0-6)?')) 
+
+day = if day_of_week = 0:
+        print ('Sunday')
+    elif day_of_week = 1:
+         print ('Monday') 
+    elif day_of_week = 2:
+        print ('Tuesday')
+    elif day_of_week = 3: 
+        print ('Wednesday')
+    elif day_of_week = 4: 
+        print ('Thursday')
+    elif day_of_week = 5:
+        print ('Friday') 
+    elif day_of_week = 6:
+        print ('Saturday')
+    else:
+        print ('Please enter a postive value between 0 and 6')
+        
+        
+        
+
+
+
+
+            
+
+print(day)

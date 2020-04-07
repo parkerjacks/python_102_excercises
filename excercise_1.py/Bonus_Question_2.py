26a49d6e244e69499405b7ee872ceae74386fef9
Park Jack <parker.jackson.hhs@gmail.com>
2020-04-07 11:50:39 -0400

    modified 2 files

--- excercise_1.py/Bonus_Question_2.py
+++ excercise_1.py/Bonus_Question_2.py
@@ -1,9 +1,9 @@
 import random
 random.randint(0,10)
 
-user_number = int(input('Type a number between 0 and 10'))
+user_number = int(input('Type a number between 0 and 10: '))
 
 if user_number == random :
     print('Go Ahead Smartie Pants!')
 else:
-  user_number
+    user_number
